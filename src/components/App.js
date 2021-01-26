2b751ec20506960df16e3e5e21625b8902ef830c
RyosukeSomeya <rx.78.0618@gmail.com>
2021-01-26 16:25:43 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import { increment, decrement } from '../actions';
 
 // クラスコンポーネント
 // class App extends Component {
@@ -15,39 +16,53 @@ import React, { Component } from 'react';
 // }
 
 // // functionalコンポーネント
-const App = () => (<Counter></Counter>)
+// const App = () => (<Counter></Counter>)
 
 // Stateはclassコンポーネントで使える
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    console.log(this.state);
-    this.state = { count: 0 };
-  }
+class App extends Component {
+  // stateの管理はReduxで行うので不要
+  // constructor(props) { // コンポーネントの初期化
+  //   super(props);
+  //   console.log(this.state);
+  //   this.state = { count: 0 };
+  // }
 
-  handlePulsButton = () => {
-    console.log("handlePulsButton");
-    this.setState({ count: this.state.count + 1 });
-    // setStateが走ると、render()がコールバックで呼ばれる（Domが再描画される）
-  }
+  // action createrとreducerで行うので不要
+  // handlePulsButton = () => {
+  //   console.log("handlePulsButton");
+  //   this.setState({ count: this.state.count + 1 });
+  //   // setStateが走ると、render()がコールバックで呼ばれる（Domが再描画される）
+  // }
 
-  handleMinusButton = () => {
-    console.log("handleMinusButton")
-    this.setState({ count: this.state.count - 1 });
-  }
+  // handleMinusButton = () => {
+  //   console.log("handleMinusButton")
+  //   this.setState({ count: this.state.count - 1 });
+  // }
 
   render() {
-    console.log('render');
-    console.log(this.state);
+    const props = this.props;
     return (
       <React.Fragment>
-        <div>count: { this.state.count }</div>
-        <button onClick={this.handlePulsButton}>+</button>
-        <button onClick={this.handleMinusButton}>-</button>
+        <div>value: { props.value }</div>
+        {/* <button onClick={this.handlePulsButton}>+</button>
+        <button onClick={this.handleMinusButton}>-</button> */}
+        <button onClick={props.increment}>+</button>
+        <button onClick={props.decrement}>-</button>
       </React.Fragment>
     );
   }
 }
 
+// mapStateToProps → stateの情報から、コンポーネントで必要なものを取り出し、コンポーネント内のpropsとしてマッピングする。
+const mapStateToProps = state => ({ value: state.count.value });
 
-export default App;
+// mapDispachToProps → あるActionが発生したときにreducerにtypeに応じた状態遷移を実行させる関数(Dispatch関数)
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement()),
+})
+
+// stateとactionをコンポーネントに関連付ける
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// export default App;
