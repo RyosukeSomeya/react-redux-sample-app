2343b81d25a8a0bde8e56449021809c688550038
RyosukeSomeya <rx.78.0618@gmail.com>
2021-01-26 13:58:50 +0900

    learn props of component 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-// // クラスコンポーネント
+// クラスコンポーネント
 // class App extends Component {
 //   render(){
 //     return (
@@ -12,9 +12,35 @@ import React, { Component } from 'react';
 //   }
 // }
 
-// functionalコンポーネント
+// // functionalコンポーネント
 const App = () => {
-  return <div>Hi!</div>
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Noname"},
+  ];
+
+  return (
+    <div>
+      {
+        profiles.map((profile, index)=> {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+      {/* Userコンポーネントにname属性を与える */}
+      {/* <User name={"Taro"} age={10} />
+      <User name={"Hanako"} age={5} /> */}
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Hi! I am { props.name }, and {props.age} years old!</div>
+}
+
+// Userコンポーネントにデフォルトのpropsを設定しておく。(デフォルト引数のようなもの)
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
